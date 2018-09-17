79d928e94db21d90a3d5f1e930df079b68ddd9fc
foxyflow <you@example.com>
2018-09-17 23:43:10 +1200

    Arrays getting pushed

---
+++ Arrays/Arrays continued/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_continued
+{
+    class Program
+    {
+        static void Main(string[] args)
+            ////////////////Arrays//////////////Luke Fox////////////////////////////Exercise 2/////////
+        {
+            string[] names = new string[5];
+            //note don't write i<= 5; (in the middle of the for loop) as array starts at zero
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"enter name {i + 1}");//// note the +1 so the console doesn't start at 0
+                names[i] = Console.ReadLine();
+            }
+            Console.ReadLine();
+            ///////////////////or///////////////////////
+            ////////////////////////
+            string[] namesAgain = new string[5];
+
+            for (int z = 0; z < namesAgain.Length; z++)   
+            {
+                Console.WriteLine($"enter name {z + 1}");///note the plus one to start at one not zero
+                namesAgain[z] = Console.ReadLine();
+            }
+            Console.WriteLine($"the first name entered was:{namesAgain [0]}");
+            Console.WriteLine($"The last on the list was {namesAgain[4]}");
+            Console.ReadLine();
+
+
+        }
+        
+    }
+}
---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welocme to Crank Worx medal awards//
+            string[] MedalWinners = new string[3];
+
+            Console.WriteLine("Please enter the Gold winner");
+            MedalWinners[0] = Console.ReadLine();
+            Console.WriteLine("Please enter the silver winner");
+            MedalWinners[1] = Console.ReadLine();
+            Console.WriteLine("Please enter the bronze winner");
+            MedalWinners[2] = Console.ReadLine();
+
+            Console.WriteLine("Which winner do you want to display? 1 for Gold, 2 for Silver, 3 for Bronze");
+            int userInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The medal winner is: {MedalWinners[userInput - 1]}");
+            Console.ReadLine();
+            //That wraps up the awards for tonight. Drinks are on me//
+
+            /////The next execise uses a for loop -- don't be scared 
+            /////what if we had more than 3 winners -- too much typing////
+        }
+    }
+}
